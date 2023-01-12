f6a0c2b298c250fe3971c90f61b0d8f57582008b
wangfu <wangfutw@gmail.com>
2023-01-12 15:56:31 +0800

    week2 rework

--- homeworks/week1/github.sh
+++ homeworks/week1/github.sh
@@ -3,9 +3,9 @@ curl https://api.github.com/users/$1 > $1.txt;
 
 echo "";
 
-grep "\"name" $1.txt | cut -c 1-11 --complement | sed "s/..$//g" ;
-grep "\"bio" $1.txt | cut -c 1-10 --complement | sed "s/..$//g" ;
-grep "\"location" $1.txt | cut -c 1-15 --complement | sed "s/..$//g" ;
-grep "\"blog" $1.txt | cut -c 1-11 --complement | sed "s/..$//g" ;
+grep '"name"' $1.txt | cut -d '"' -f 4;
+grep '"bio"' $1.txt | cut -d '"' -f 4 ;
+grep '"location"' $1.txt | cut -d '"' -f 4 ;
+grep '"blog"' $1.txt | cut -d '"' -f 4 ;
 
 rm $1.txt;
